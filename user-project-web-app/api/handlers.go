4b82bb7ef2fe85096cfdd714fae3932953212953
Amdjed Belgacem <45467302+AmdjedBelgacem@users.noreply.github.com>
2024-01-18 23:53:54 +0100

    migrating from sqlite to mongodb to host it online

--- user-project-web-app/api/handlers.go
+++ user-project-web-app/api/handlers.go
@@ -1,155 +1,177 @@
-// This is to Indicate that this file is part of the main package.
 package main
 
-// Importing necessary packages for the server functionality.
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux" // Importing the Gorilla Mux router package for handling HTTP requests.
-	"gorm.io/driver/sqlite"  // Importing the SQLite driver for GORM.
-	"gorm.io/gorm"           // Importing the GORM library for database interaction.
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Global variables to hold the GORM database connection and potential initialization error.
-var db *gorm.DB
-var err error
+var client *mongo.Client
 
 // Initialization function that is executed once when the program starts.
 func init() {
-	// Opening a connection to an SQLite database file named "user.db".
-	db, err = gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
+	// Set up the MongoDB client
+	clientOptions := options.Client().ApplyURI("mongodb+srv://AmdjdBelgacem:67x7BP23GsAtYbS9@user-management-cluster.pmkcszj.mongodb.net/?retryWrites=true&w=majority")
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	// Automatically applying any pending migrations to the User model.
-	db.AutoMigrate(&User{})
+	// Check the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Connected to MongoDB")
+
+	// Automatically create an index for the "username" field
+	indexOptions := options.Index().SetUnique(true)
+	usernameIndex := mongo.IndexModel{
+		Keys:    bson.M{"username": 1},
+		Options: indexOptions,
+	}
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	_, err = userCollection.Indexes().CreateOne(context.Background(), usernameIndex)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 // Handler function to retrieve all users from the database.
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	// Declaring a slice to hold user objects.
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	cursor, err := userCollection.Find(context.Background(), bson.M{})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	defer cursor.Close(context.Background())
+
 	var users []User
+	err = cursor.All(context.Background(), &users)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
 
-	// Querying the database to retrieve all user records.
-	db.Find(&users)
-
-	// Setting the HTTP response status code to 200 (OK).
 	w.WriteHeader(http.StatusOK)
-
-	// Encoding the retrieved user data as JSON and writing it to the response.
-	json.NewEncoder(w).Encode(&users)
+	json.NewEncoder(w).Encode(users)
 }
 
 // Handler function to retrieve a user by ID from the database.
 func GetUserByID(w http.ResponseWriter, r *http.Request) {
-	// Extracting parameters from the request URL, I'm targeting the User Id in this case.
 	params := mux.Vars(r)
-
-	// Declaring a variable to hold the user object.
-	var user User
-
-	// Querying the database to retrieve a user by their ID.
-	result := db.First(&user, params["id"])
-
-	// Checking if the query result contains an error (user not found).
-	if result.Error != nil {
-		// Setting the HTTP response status code to 404 (Not Found).
-		w.WriteHeader(http.StatusNotFound)
-	} else {
-		// Setting the HTTP response status code to 200 (OK).
-		w.WriteHeader(http.StatusOK)
-
-		// Encoding the retrieved user data as JSON and writing it to the response.
-		json.NewEncoder(w).Encode(&user)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		return
 	}
+
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	var user User
+	err = userCollection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Println(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
 
 // Handler function to create a new user in the database.
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	// Declaring a variable to hold the user object.
 	var user User
-
-	// Decoding the JSON request body into the user object.
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		// Setting the HTTP response status code to 400 (Bad Request) if decoding fails.
 		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
 		return
 	}
 
-	// Creating a new user record in the database.
-	db.Create(&user)
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	_, err = userCollection.InsertOne(context.Background(), user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
 
-	// Setting the HTTP response status code to 201 (Created).
 	w.WriteHeader(http.StatusCreated)
-
-	// Encoding the created user data as JSON and writing it to the response.
-	json.NewEncoder(w).Encode(&user)
+	json.NewEncoder(w).Encode(user)
 }
 
 // Handler function to update a user in the database by ID.
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	// Extracting parameters (in this case, the "id" parameter) from the request URL.
 	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
 
-	// Declaring a variable to hold the user object.
-	var user User
+	var updatedUser User
+	err = json.NewDecoder(r.Body).Decode(&updatedUser)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
 
-	// Querying the database to retrieve a user by their ID.
-	result := db.First(&user, params["id"])
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	result, err := userCollection.UpdateOne(context.Background(), bson.M{"_id": id}, bson.M{"$set": updatedUser})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
 
-	// Checking if the query result contains an error (user not found).
-	if result.Error != nil {
-		// Setting the HTTP response status code to 404 (Not Found).
+	if result.ModifiedCount == 0 {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	// Decoding the JSON request body into the user object.
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		// Setting the HTTP response status code to 400 (Bad Request) if decoding fails.
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Saving the updated user record in the database.
-	db.Save(&user)
-
-	// Setting the HTTP response status code to 200 (OK).
 	w.WriteHeader(http.StatusOK)
-
-	// Encoding the updated user data as JSON and writing it to the response.
-	json.NewEncoder(w).Encode(&user)
+	json.NewEncoder(w).Encode(updatedUser)
 }
 
 // Handler function to delete a user from the database by ID.
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	// Extracting parameters (in this case, the "id" parameter) from the request URL.
 	params := mux.Vars(r)
-
-	// Converting the "id" parameter to an integer.
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		// Setting the HTTP response status code to 400 (Bad Request) if conversion fails.
 		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
 		return
 	}
 
-	// Deleting the user record from the database by ID.
-	result := db.Delete(&User{}, id)
+	userCollection := client.Database("user-management-cluster").Collection("users")
+	result, err := userCollection.DeleteOne(context.Background(), bson.M{"_id": id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
 
-	// Checking if the delete operation affected any rows (user not found).
-	if result.RowsAffected == 0 {
-		// Setting the HTTP response status code to 404 (Not Found).
+	if result.DeletedCount == 0 {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	// Setting the HTTP response status code to 204 (No Content) as the resource is successfully deleted.
 	w.WriteHeader(http.StatusNoContent)
 }
--- user-project-web-app/api/models.go
+++ user-project-web-app/api/models.go
@@ -2,21 +2,14 @@
 package main
 
 // Importing the GORM library for it to work with the database.
-import (
-	"github.com/jinzhu/gorm"
-)
 
 // Defining a User struct, which represents a user entity in the database.
 type User struct {
-	// Embedding the gorm.Model to include common fields like ID, CreatedAt, UpdatedAt, and DeletedAt.
-	gorm.Model
-	// Username field with a unique constraint, indicating that each username must be unique in the database.
-	Username string `gorm:"unique_index"`
-	// Each Field represents itself in the database for the user's information.
-	FullName string
-	// Email field with a unique constraint, indicating that each email must be unique in the database.
-	Email       string `gorm:"unique_index"`
-	Gender      string
-	BirthDate   string
-	PhoneNumber string
+	ID          int    `json:"id" bson:"_id,omitempty"`
+	Username    string `json:"username" bson:"username"`
+	FullName    string `json:"fullName" bson:"fullName"`
+	Email       string `json:"email" bson:"email"`
+	Gender      string `json:"gender" bson:"gender"`
+	BirthDate   string `json:"birthDate" bson:"birthDate"`
+	PhoneNumber string `json:"phoneNumber" bson:"phoneNumber"`
 }
